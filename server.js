8f4a64ebf3764ae60c3055e54a31dedc92bf2b7d
Sky-dex <alejomop28@gmail.com>
2026-05-16 21:31:33 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
+
+app.get('/temperatura', (req, res) => {
+    const datosTemperatura = [
+        { mes: 'Enero', valor: 24},
+     { mes: 'Febrero', valor: 25},
+     { mes: 'Marzo', valor: 26},
+     { mes: 'Abril', valor: 27},
+     { mes: 'Mayo', valor: 28},
+     { mes: 'Junio', valor: 27},
+     { mes: 'Julio', valor: 25},
+     { mes: 'Agosto', valor: 35},
+     { mes: 'Septiembre', valor: 20},
+     { mes: 'Octubre', valor: 22},
+     { mes: 'Noviembre', valor: 26},
+     { mes: 'Diciembre', valor: 30},
+    ];
+    res.json(datosTemperatura);
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en puerto ${PORT}`);
 });
